81e797e48b659f9f604033ea8307dbdc850d3271
--global <wallyssonryu@gmail.com>
2026-08-26 22:49:43 -0300

    FEAT: new channel

--- files/.config/guix/channels.scm
+++ files/.config/guix/channels.scm
@@ -7,4 +7,8 @@
     "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
     (openpgp-fingerprint
      "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
- %default-channels)
+   (channel
+    (name 'argent)
+    (url "https://codeberg.org/z-silver/argent")
+    (branch "master"))
+   %default-channels)
