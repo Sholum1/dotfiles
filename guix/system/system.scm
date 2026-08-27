c2fef57add834842d11a1e17c707879d03770b02
--global <wallyssonryu@gmail.com>
2026-08-26 23:23:23 -0300

    FIX?

--- guix/system/system.scm
+++ guix/system/system.scm
@@ -34,16 +34,16 @@
                   (group "users")
                   (home-directory "/home/Sholum")
                   (supplementary-groups '("wheel" "netdev" "audio"
-					  "video" "cgroup" "kvm"
-					  "libvirt" "dialout")))
+                                          "video" "cgroup" "kvm"
+                                          "libvirt" "dialout")))
                 %base-user-accounts))
 
   ;; Packages installed system-wide.  Users can also install packages
   ;; under their own account: use 'guix search KEYWORD' to search
   ;; for packages and 'guix install PACKAGE' to install a package.
   (packages (append (list (specification->package "emacs")
-			  (specification->package "efibootmgr"))
-		    %base-packages))
+                          (specification->package "efibootmgr"))
+                    %base-packages))
 
   ;; Below is the list of system services.  To search for available
   ;; services, run 'guix system search KEYWORD' in a terminal.
@@ -54,36 +54,36 @@
                  (service openssh-service-type)
                  (service tor-service-type)
                  (service cups-service-type)
-		 (service iptables-service-type)
-		 (service rootless-podman-service-type
-			  (rootless-podman-configuration
-			   (subgids
-			    (list (subid-range (name "Sholum"))))
-			   (subuids
-			    (list (subid-range (name "Sholum"))))))
-		 (simple-service 'podman-subuid-subgid
-				 subids-service-type
-				 (subids-extension
-				  (subgids
-				   (list (subid-range (name "Sholum"))))
-				  (subuids
-				   (list (subid-range (name "Sholum"))))))
-		 (service libvirt-service-type)
-		 (set-xorg-configuration
-		  (xorg-configuration (keyboard-layout keyboard-layout))))
+                 (service iptables-service-type)
+                 (service rootless-podman-service-type
+                          (rootless-podman-configuration
+                           (subgids
+                            (list (subid-range (name "Sholum"))))
+                           (subuids
+                            (list (subid-range (name "Sholum"))))))
+                 (simple-service 'podman-subuid-subgid
+                                 subids-service-type
+                                 (subids-extension
+                                  (subgids
+                                   (list (subid-range (name "Sholum"))))
+                                  (subuids
+                                   (list (subid-range (name "Sholum"))))))
+                 (service libvirt-service-type)
+                 (set-xorg-configuration
+                  (xorg-configuration (keyboard-layout keyboard-layout))))
 
            ;; This is the default list of services we
            ;; are appending to.
            (modify-services %desktop-services
-			    (guix-service-type config =>
-					       (guix-configuration
-						(inherit config)
-						(substitute-urls
-						 (append (list "https://substitutes.nonguix.org")
-							 %default-substitute-urls))
-						(authorized-keys
-						 (append (list (local-file "./signing-key.pub"))
-							 %default-authorized-guix-keys)))))))
+                            (guix-service-type config =>
+                                               (guix-configuration
+                                                (inherit config)
+                                                (substitute-urls
+                                                 (append (list "https://substitutes.nonguix.org")
+                                                         %default-substitute-urls))
+                                                (authorized-keys
+                                                 (append (list (local-file "./signing-key.pub"))
+                                                         %default-authorized-guix-keys)))))))
   (bootloader (bootloader-configuration
                 (bootloader grub-efi-removable-bootloader)
                 (targets (list "/boot/efi"))
@@ -98,24 +98,25 @@
                                   "72267a24-e934-46d8-8a17-5c89ab622c7f"
                                   'ext4))
                          (type "ext4")
-			 (flags '(no-atime)))
+                         (flags '(no-atime)))
                        (file-system
                          (mount-point "/")
                          (device (uuid
                                   "4079f732-9855-4a4a-893e-2642664fb301"
                                   'ext4))
                          (type "ext4")
-			 (flags '(no-atime)))
+                         (flags '(no-atime)))
                        (file-system
                          (mount-point "/home/Sholum/sata")
                          (device (uuid
                                   "f8b62d74-b863-48fa-a048-374ed0a67e5a"
                                   'ext4))
                          (type "ext4")
-			 (flags '(no-atime)))
-		       (file-system
+                         (create-mount-point? #t)
+                         (flags '(no-atime)))
+                       (file-system
                          (mount-point "/boot/efi")
                          (device (uuid "55CD-33E5"
                                        'fat32))
                          (type "vfat"))
-		       %base-file-systems)))
+                       %base-file-systems)))
